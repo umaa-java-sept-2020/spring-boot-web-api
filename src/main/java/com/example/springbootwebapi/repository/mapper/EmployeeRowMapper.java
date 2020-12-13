ca4ce659e895f15a1f9689adad48ec4aec92497e
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-13 18:10:48 +0530

    added repository layer for queries

---
+++ src/main/java/com/example/springbootwebapi/repository/mapper/EmployeeRowMapper.java
@@ -0,0 +1,23 @@
+package com.example.springbootwebapi.repository.mapper;
+
+import com.example.springbootwebapi.model.Employee;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component("employeeRowMapper")
+public class EmployeeRowMapper implements RowMapper<Employee> {
+
+    @Override
+    public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Integer id = resultSet.getInt("ID");
+        String uid = resultSet.getString("UID");
+        String firstName = resultSet.getString("FIRST_NAME");
+        String lastName = resultSet.getString("LAST_NAME");
+        Employee e = new Employee(id, uid, firstName, lastName);
+        return e;
+    }
+}
