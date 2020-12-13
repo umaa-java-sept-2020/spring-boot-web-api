09a1140e689de46e220ef926ca3d626453003b3f
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-13 18:00:47 +0530

    added employee controller to say hello /employees/hello

---
+++ src/main/java/com/example/springbootwebapi/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.example.springbootwebapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/employees/hello")
+    public String saySomething()
+    {
+        return "I am learning spring boot";
+    }
+}
